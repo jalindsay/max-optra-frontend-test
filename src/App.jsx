cf89d7a269eea32b5b26cf74b40b281a0d0c082f
James Lindsay <james.lindsay@polestar-eam.com>
2024-08-01 17:24:24 +0100

    Added package to generate all random data, including addresses. Fixed endDate timestamp so it goes up till end of day.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Box, Card, CardContent, Typography, TextField } from "@mui/material";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import Fakerator from 'fakerator';
+let fakerator = Fakerator("en-GB");
 
 // Generate a random unix time between now and the past 2 years
 const getRandomUnixTime = () => {
@@ -17,18 +19,21 @@ const generateInitialOrders = () => {
       console.log(`generating order ${i}`)
     }
     // Using a random starting timestamp then set hour intervals for testing times
-    let startTime1 = getRandomUnixTime(); // Random timestamp between now and the past 2 years
+    let startTime1 = getRandomUnixTime(); // Random timestamp between now and 2 years ago
     let endTime1 = startTime1 + 3600; // 1 hour later
     let startTime2 = startTime1 + 18000; // 5 hours later
     let endTime2  = startTime2 + 3600; // 6 hours later
+
+    let address = fakerator.address.street() + ", " + fakerator.address.postCode() + ", " + fakerator.address.country();
+
     let order = {
       id: i,
-      name: `Test Order ${i}`,
+      name: `Test Order ${i} ${fakerator.names.lastName()}`,
       location: {
-        name: `Test Location ${i}`,
-        longitude: 0,
-        latitude: 0,
-        address: ""
+        name: `Test Location ${i} ${fakerator.address.city()}`,
+        longitude: fakerator.address.geoLocation().longitude,
+        latitude: fakerator.address.geoLocation().longitude,
+        address: address
       },
       dropWindows: [
         {
@@ -89,11 +94,12 @@ function App() {
         orderEndTime2 >= new Date(startTimeFilter).getTime() / 1000
     ) : true;
 
+    // Match end time. Add 86400 seconds to end time to match end of day
     const endTimeMatch = endTimeFilter ? (
-        orderStartTime1 <= new Date(endTimeFilter).getTime() / 1000 ||
-        orderEndTime1 <= new Date(endTimeFilter).getTime() / 1000 ||
-        orderStartTime2 <= new Date(endTimeFilter).getTime() / 1000 ||
-        orderEndTime2 <= new Date(endTimeFilter).getTime() / 1000
+        orderStartTime1 <= ((new Date(endTimeFilter).getTime() / 1000) + 86400) ||
+        orderEndTime1 <= ((new Date(endTimeFilter).getTime() / 1000) + 86400) ||
+        orderStartTime2 <= ((new Date(endTimeFilter).getTime() / 1000) + 86400) ||
+        orderEndTime2 <= ((new Date(endTimeFilter).getTime() / 1000) + 86400)
     ) : true;
 
     return order.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
@@ -101,9 +107,9 @@ function App() {
         endTimeMatch;
   });
 
-  // TODO: add generated addresses
-  // TODO: make date filters start from beginning of day and end of day
   // TODO: format drop windows to only display ending time. (01/01/2020 10:00-11:00)
+  // TODO: make card column fixed width
+  // TODO: DatePicker state controll warning
   return (
     <div className="App">
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
