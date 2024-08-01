e7f8e6b8bf064787bc6423c71dcec2a64f9ced40
James Lindsay <james.lindsay@polestar-eam.com>
2024-08-01 02:11:33 +0100

    gitignore update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
+
       <App />
     </StyledEngineProvider>
   </React.StrictMode>
